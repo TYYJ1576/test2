c6dabeff2f9c284bf424d5b82ef28a94ee31965f
TYYJ1576 <130568371+TYYJ1576@users.noreply.github.com>
2023-08-15 17:36:27 +0800

    Update main.ts

--- main.ts
+++ main.ts
@@ -14,6 +14,17 @@ namespace PI {
         OFF
     }
 
+    enum Distance_Unit {
+        //% block="mm" enumval=0
+        Distance_Unit_mm,
+
+        //% block="cm" enumval=1
+        Distance_Unit_cm,
+
+        //% block="inch" enumval=2
+        Distance_Unit_inch,
+    }
+
     export enum Boo {
         //% block="True"
         T,
@@ -162,6 +173,102 @@ namespace PI {
         } else if (pins.digitalReadPin(DigitalPin.P1) == 1 && pins.digitalReadPin(DigitalPin.P15) == 1) {
             setMotorSpeed(MotorList.M1, -speed)
             setMotorSpeed(MotorList.M2, speed)
+        }
+    }
+
+    //% weight=81
+    //% blockId=sonarbit  block="Ultrasonic distance in unit %distance_unit |at|pin %pin"
+    export function sonarbit_distance(distance_unit: Distance_Unit, pin: DigitalPin): number {
+
+        let old = 0
+        let now = 0
+        // send pulse
+        pins.setPull(pin, PinPullMode.PullNone)
+        pins.digitalWritePin(pin, 0)
+        control.waitMicros(2)
+        pins.digitalWritePin(pin, 1)
+        control.waitMicros(10)
+        pins.digitalWritePin(pin, 0)
+
+        // read pulse
+        let d = pins.pulseIn(pin, PulseValue.High, 25000)  // 8 / 340 = 
+
+        let distance = d / 58
+
+        if (distance > 400) {
+            distance = 0
+        }
+
+        switch (distance_unit) {
+            case 0:
+                return Math.floor(distance * 10) //mm
+                break
+            case 1:
+                return Math.floor(distance)  //cm
+                break
+            case 2:
+                return Math.floor(distance / 254)   //inch
+                break
+            default:
+                return 0
+        }
+
+    }
+
+    //% weight=80
+    //% blockId=setServoAngle block="Set %servoType servo %servo angle to %angle"
+    //% angle.min=0 angle.max=360
+    export function setServoAngle(servoType: ServoTypeList, servo: ServoList, angle: number): void {
+        let buf = pins.createBuffer(4);
+        if (servo == 0) {
+            buf[0] = 0x03;
+        }
+        if (servo == 1) {
+            buf[0] = 0x04;
+        }
+        if (servo == 2) {
+            buf[0] = 0x05;
+        }
+        if (servo == 3) {
+            buf[0] = 0x06;
+        }
+        if (servo == 4) {
+            buf[0] = 0x07;
+        }
+        if (servo == 5) {
+            buf[0] = 0x08;
+        }
+        if (servo == 6) {
+            buf[0] = 0x09;
+        }
+        if (servo == 7) {
+            buf[0] = 0x10;
+        }
+
+        switch (servoType) {
+            case ServoTypeList._180:
+                angle = Math.map(angle, 0, 180, 0, 180)
+                break
+            case ServoTypeList._270:
+                angle = Math.map(angle, 0, 270, 0, 180)
+                break
+            case ServoTypeList._360:
+                angle = Math.map(angle, 0, 360, 0, 180)
+                break
+        }
+
+        buf[1] = angle;
+        buf[2] = 0;
+        buf[3] = 0;
+        pins.i2cWriteBuffer(board_address, buf);
+    }
+
+    //% weight=79
+    //% blockId=autoGrab  block="auto grab set to %boo within distance %distance at pin %"
+    //% distance.min=0 distance.max=100
+    export function autoGrab(boo: Boo, distance: number, ): void{
+        if (sonarbit_distance(Distance_Unit.Distance_Unit_mm, DigitalPin.P0) < 10) {
+            setServoAngle(ServoTypeList._180, ServoList.S0, 0)
         }
     }
 }
